ac94e9f665423677793411c11057efba9c623ee7
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-07-10 20:46:57 +0900

    mod deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,41 +1,20 @@
 #!/bin/sh
-# pre bench
-# cleaup mysql log
-sudo rm /var/log/mysql/mysql-slow.log
-sudo mysqladmin flush-logs
-
-# restart nginx
-sudo rm /var/log/nginx/access.log && sudo systemctl reload nginx
-# run bench before pull commit
-git checkout main
-# write run bench command
-
-
-# output alp
-sudo cat /var/log/nginx/access.log | alp ltsv
-
-# output mysql log
-sudo mysqldumpslow /var/log/mysql/mysql-slow.log
-sudo pt-query-digest /var/log/mysql/mysql-slow.log
-
-
 # pull commit
 git pull
 git checkout $1
 
-# post bench
+# write webapp restart command
+cd webapp/go && go build
+sudo systemctl restart isucondition.go.service
+
+# bench
 # cleaup mysql log
 sudo rm /var/log/mysql/mysql-slow.log
 sudo mysqladmin flush-logs
+sudo systemctl restart mysql
 
 # restart nginx
 sudo rm /var/log/nginx/access.log && sudo systemctl reload nginx
-
-# write webapp restart command
-sudo systemctl restart isucondition.go.service
-
-# write sql restart command
-sudo systemctl restart mysql
 
 # write run bench command
 
