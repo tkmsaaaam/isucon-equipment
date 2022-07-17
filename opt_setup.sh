27b1c47320102fece13c106635d3b7f84256fce8
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2022-07-17 15:47:03 +0900

    add opt_setup.sh

---
+++ opt_setup.sh
@@ -0,0 +1,15 @@
+# nginx
+sudo cp /etc/nginx/nginx.conf ~/nginx.conf
+sudo rm /etc/nginx/nginx.conf
+sudo ln -s ~/nginx.conf /etc/nginx/nginx.conf
+sudo cat /etc/nginx/nginx.conf
+
+# mysql
+sudo cp /etc/my.cnf ~/my.cnf
+sudo rm /etc/my.cnf
+sudo ln -s ~/my.cnf /etc/my.cnf
+
+# # extra optional mysql
+# sudo cp /etc/mysql/my.cnf ~/my.cnf
+# sudo rm /etc/mysql/my.cnf
+# sudo ln -s ~/my.cnf etc/mysql/my.cnf
